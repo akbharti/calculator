d5f73308b8ab1cbe42ba606f01252cbcd8b857d7
ABHISHEK BHARTI <ABHISHEK BHARTI@Abhishek>
2018-05-31 14:03:49 +0530

    this is  calculator commit 1

---
+++ testcalculator/src/testCalculator.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author ABHISHEK BHARTI
+ */
+class testCalculator {
+
+    public testCalculator() {
+    }
+
+    int add(int i, int i0) {
+        //throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        return i+i0;
+    }
+    
+    int sub(int i, int i0) {
+        //throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        return i-i0;
+    }
+    
+    int mul(int i, int i0) {
+        //throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        return i*i0;
+    }
+    
+    int div(int i, int i0) {
+        //throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        return i/i0;
+    }
+}
